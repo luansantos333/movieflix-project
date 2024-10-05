5efb0274601b9974338289eaa2661abc7281c79e
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-10-05 18:15:38 -0300

    feat: add component scan

--- src/main/java/org/dev/luan/moviesite/ProjectMoviesSiteApplication.java
+++ src/main/java/org/dev/luan/moviesite/ProjectMoviesSiteApplication.java
@@ -1,9 +1,14 @@
 package org.dev.luan.moviesite;
 
+import org.hibernate.annotations.Comment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan (basePackages = "org.dev.luan.moviesite")
+
 public class ProjectMoviesSiteApplication {
 
 	public static void main(String[] args) {
