2cac216e7195c17b01a46dd7e5dde4e62d2d4fda
luangomes <luangomespctu@gmail.com>
2024-10-19 16:26:57 -0300

    feat: add findAllMoviesSortedByName method

--- src/main/java/org/dev/luan/moviesite/services/MovieService.java
+++ src/main/java/org/dev/luan/moviesite/services/MovieService.java
@@ -1,7 +1,9 @@
 package org.dev.luan.moviesite.services;
 
 import org.dev.luan.moviesite.dtos.MovieDTO;
+import org.dev.luan.moviesite.dtos.MovieMinDTO;
 import org.dev.luan.moviesite.repositories.MovieRepository;
+import org.dev.luan.moviesite.repositories.projections.MovieProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,9 +16,20 @@ public class MovieService {
     private MovieRepository repository;
 
 
+    /*
     @Transactional (readOnly = true)
     public Page<MovieDTO> findAllMoviesO (Pageable p) {
         return repository.listAllMoviesPaged(p).map(MovieDTO::new);
+    }
+    */
+    @Transactional (readOnly = true)
+    public Page<MovieMinDTO> findAllMoviesSortedByName (Pageable p) {
+
+
+        Page<MovieProjection> movieProjections = repository.listAllMoviesPaged(p);
+
+        return movieProjections.map(MovieMinDTO::new);
+
     }
 
 
