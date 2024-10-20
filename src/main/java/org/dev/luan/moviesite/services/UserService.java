211355df374e05a2af78bdac55caea64d928b98d
luangomes <luangomespctu@gmail.com>
2024-10-20 18:57:45 -0300

    feat: add deleteUser and loadByUsername method

--- src/main/java/org/dev/luan/moviesite/services/UserService.java
+++ src/main/java/org/dev/luan/moviesite/services/UserService.java
@@ -6,12 +6,20 @@ import org.dev.luan.moviesite.entities.Role;
 import org.dev.luan.moviesite.entities.User;
 import org.dev.luan.moviesite.repositories.RoleRepository;
 import org.dev.luan.moviesite.repositories.UserRepository;
+import org.dev.luan.moviesite.repositories.projections.UserDetailsProjection;
+import org.dev.luan.moviesite.services.exceptions.DatabaseException;
+import org.dev.luan.moviesite.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -31,10 +39,56 @@ public class UserService implements UserDetailsService {
         return new UserDTO(entity);
     }
 
+    @Transactional (propagation = Propagation.SUPPORTS)
+    public void deleteUser (Long id) {
+
+        if (!userRepository.existsById(id)) {
+
+            throw new ResourceNotFoundException("O usuário informado não foi encontrado");
+
+        }
+
+        try {
+
+            userRepository.deleteById(id);
+
+
+        } catch (DataIntegrityViolationException e) {
+
+            throw new DatabaseException("O recurso que você está tentando deletar está vinculado a outra tabela");
+
+        }
+
+
+    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        List<UserDetailsProjection> projections = userRepository.searchUserAndRolesByEmail(username);
+
+        if (projections.isEmpty()) {
+
+            throw new ResourceNotFoundException("Não foi encontrado nenhum usuário com o email informado");
+
+        }
+
+        User user = new User();
+        user.setEmail(projections.get(0).getUsername());
+        user.setPassword(projections.get(0).getPassword());
+
+        for (UserDetailsProjection p : projections) {
+
+
+            user.addNewRole(new Role(p.getRoleId(), p.getAuthority()));
+
+
+        }
+
+
+        return user;
+
     }
 
 
