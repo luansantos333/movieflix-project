cf46936fb0e40cabaa1c3260d532e942556d07a5
luangomes <luangomespctu@gmail.com>
2024-10-20 10:51:12 -0300

    feat: add ResourceNotFound Exception

---
+++ src/main/java/org/dev/luan/moviesite/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package org.dev.luan.moviesite.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
