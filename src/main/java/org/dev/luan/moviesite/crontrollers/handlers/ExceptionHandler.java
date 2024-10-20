a8a574ae427e41a5315a4882fd82cf06de5a7a61
luangomes <luangomespctu@gmail.com>
2024-10-20 10:49:30 -0300

    feat: add exception handler classes

---
+++ src/main/java/org/dev/luan/moviesite/crontrollers/handlers/ExceptionHandler.java
@@ -0,0 +1,24 @@
+package org.dev.luan.moviesite.crontrollers.handlers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.dev.luan.moviesite.services.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+
+    @org.springframework.web.bind.annotation.ExceptionHandler (ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> handlerResourceNotFound (ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), status.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+
+}
