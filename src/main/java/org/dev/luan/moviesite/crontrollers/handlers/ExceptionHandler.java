f48eca661900fee4170a5713ce15d24d5419ca58
luangomes <luangomespctu@gmail.com>
2024-10-20 18:56:09 -0300

    feat: add DatabaseException handler

--- src/main/java/org/dev/luan/moviesite/crontrollers/handlers/ExceptionHandler.java
+++ src/main/java/org/dev/luan/moviesite/crontrollers/handlers/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.dev.luan.moviesite.crontrollers.handlers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.dev.luan.moviesite.services.exceptions.DatabaseException;
 import org.dev.luan.moviesite.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,17 @@ public class ExceptionHandler {
 
 
     @org.springframework.web.bind.annotation.ExceptionHandler (ResourceNotFoundException.class)
-    public ResponseEntity<CustomError> handlerResourceNotFound (ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<CustomError> handleResourceNotFound (ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), status.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler (DatabaseException.class)
+    public ResponseEntity<CustomError>  handleDatabaseException (DatabaseException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.CONFLICT;
         CustomError err = new CustomError(Instant.now(), status.value(), status.getReasonPhrase(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
 
