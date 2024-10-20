7aefa2115675489cc192a684b27950ecd840fbed
luangomes <luangomespctu@gmail.com>
2024-10-20 10:57:18 -0300

    feat: add projection class

---
+++ src/main/java/org/dev/luan/moviesite/repositories/projections/MovieReviewUserProjection.java
@@ -0,0 +1,22 @@
+package org.dev.luan.moviesite.repositories.projections;
+
+import java.util.List;
+
+public interface MovieReviewUserProjection {
+
+    String getTitle();
+
+    String getSubTitle();
+
+    String getSynopsis();
+
+    Integer getMovieYear();
+
+    String getText();
+
+    String getUsername();
+
+    String getImgUrl();
+
+
+}
